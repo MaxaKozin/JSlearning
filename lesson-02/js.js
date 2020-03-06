6b115daa28874cfba79a2885ebee8b2cfddaa546
Maxim Kozin <maxa.kozin@gmail.com>
2020-03-06 17:28:47 +0300

    lesson3 initial commit

--- lesson-02/js.js
+++ lesson-02/js.js
@@ -1,7 +1,55 @@
 'use strict';
 
-let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+
+function detectExpences() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+            b = +prompt('Во сколько обойдется?', '');
+
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log('all OK');
+            appData.expences[a] = b;
+        } else {
+            console.log('Error!');
+            i--;
+        };
+    };
+}
+
+function detectLevel () {
+    if (moneyPerDay < 100) {
+        console.log('So low');
+    } else if (moneyPerDay > 100 && moneyPerDay < 2000) {
+        console.log('Medium salary');
+    } else if (moneyPerDay > 2000) {
+        console.log('High salary');
+    } else {
+        console.log('Error');
+    };
+}
+
+function detectOptionalExpences() {
+    for (let i = 0; i < 3; i++) {
+        let a = +prompt('Введите статью необязательных расходов:', ''),
+            if ((typeof (a)) === 'number' && (typeof (a)) != null && a != '' && a.length < 50) {
+            console.log('all OK');
+            appData.expences[a] = b;
+        } else {
+            console.log('Error!');
+            i--;
+        };
+    };
+}
+
+let money, time, moneyPerDay, optionalExpences;
 
 let appData = {
     budget: money,
@@ -12,18 +60,13 @@ let appData = {
     savings: false
 };
 
-expencesInput: for (let i = 0; i < 2; i++) {
-    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-        b = +prompt('Во сколько обойдется?', '');
+function detectDayBudget() {
+    appData.moneyPerDay = money / 30;
+    alert("Бюджет на один день составляет: " + appData.moneyPerDay);
+}
 
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log('all OK');
-        appData.expences[a] = b;
-    } else {
-        console.log('Error!');
-        i--;
-    };
-};
+
+
 
 // let i = 0;
 // while (i < 2) {
@@ -55,16 +98,9 @@ expencesInput: for (let i = 0; i < 2; i++) {
 //     };
 //     i++;
 // } while (i < 2);
+start();
+detectExpences();
+detectDayBudget();
+detectLevel();
 
-appData.moneyPerDay = appData.budget / 30;
-alert("Бюджет на один день составляет: " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
-    console.log('So low');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Medium salary');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('High salary');
-} else {
-    console.log('Error');
-};
